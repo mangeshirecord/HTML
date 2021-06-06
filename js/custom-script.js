a53b689eef30ffc9707e5bc0f1cb56d6aa338331
atith126 <45619110+atith126@users.noreply.github.com>
2021-06-06 23:15:47 +0530

    Left Menu & 2 New Master

---
+++ js/custom-script.js
@@ -0,0 +1,34 @@
+function setmenu(tp) {
+    var menu_list = $(".metismenu > li").length;
+    for (i = 1; i <= menu_list; i++) {
+        if (eval(i) == tp) 
+    {
+      if(document.getElementsByClassName("topmenu_" + i))
+            $(".topmenu_" + i).addClass("mm-active");
+            $(".topmenu_" + i + " ul").addClass("mm-show");
+        }
+    else 
+    {
+      if(document.getElementsByClassName("topmenu_" + i))
+            $(".topmenu_" + i).removeClass("mm-active");
+            $(".topmenu_" + i + " ul").removeClass("mm-show");
+        }
+    }
+}
+
+
+function setsubmenu(tp_1) {
+    var menu_list_1 = $(".sub-menu li").length;
+    for (i = 1; i <= menu_list_1; i++) {
+        if (eval(i) == tp_1) 
+    {
+      if(document.getElementsByClassName("submenu_" + i))
+            $(".submenu_" + i).addClass("mm-active");
+        }
+    else 
+    {
+      if(document.getElementsByClassName("submenu_" + i))
+            $(".submenu_" + i).removeClass("mm-active");
+        }
+    }
+}
